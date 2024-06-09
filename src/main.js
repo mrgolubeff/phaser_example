24d0338e6595e50669f5f47e5cae56393da6d757
Vladimir Golubev <mrgolubeff@icloud.com>
2024-06-09 21:47:36 +0300

    Preload all assets & place a star on scene

--- src/main.js
+++ src/main.js
@@ -5,11 +5,19 @@ class Example extends Phaser.Scene
     preload ()
     {
         this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude',
+            'assets/dude.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
     }
 
     create ()
     {
         this.add.image(400, 300, 'sky');
+        this.add.image(400, 300, 'star');
     }
 }
 
