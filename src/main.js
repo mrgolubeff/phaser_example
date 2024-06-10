d5f4f34e426e0dc1a88196a99a716b3b24a05465
Vladimir Golubev <mrgolubeff@icloud.com>
2024-06-10 12:12:15 +0300

    Create controls for player

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'phaser';
 
 var player;
 var platforms;
+var cursors;
 
 class Example extends Phaser.Scene
 {
@@ -33,6 +34,57 @@ class Example extends Phaser.Scene
 
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
+
+        this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(player, platforms);
+    }
+
+    update ()
+    {
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-160);
+
+            player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(160);
+
+            player.anims.play('right', true);
+        }
+        else
+        {
+            player.setVelocityX(0);
+
+            player.anims.play('turn');
+        }
+
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.setVelocityY(-330);
+        }
     }
 }
 
@@ -44,7 +96,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 300 }
         }
     },
 };
