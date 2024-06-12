3f2031b193104ed7069168050a9e6c7a4081f83b
Vladimir Golubev <mrgolubeff@icloud.com>
2024-06-12 11:47:32 +0300

    Spawn bombs that kill player

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ var cursors;
 var stars;
 var score = 0;
 var scoreText;
+var bombs;
 
 class Example extends Phaser.Scene
 {
@@ -82,6 +83,34 @@ class Example extends Phaser.Scene
 
             score += 10;
             scoreText.setText('score: ' + score);
+
+            if (stars.countActive(true) === 0)
+            {
+                stars.children.iterate(
+                    function (child) {
+                        child.enableBody(true, child.x, 0, true, true);
+                    }
+                );
+
+                var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+                var bomb = bombs.create(x, 16, 'bomb');
+                bomb.setBounce(1);
+                bomb.setCollideWorldBounds(true);
+                bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+            }
+        }
+
+        bombs = this.physics.add.group();
+        this.physics.add.collider(bombs, platforms);
+        this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+        function hitBomb (player, bomb)
+        {
+            this.physics.pause();
+            player.setTint(0xff0000);
+            player.anims.play('turn');
+            // gameOver = true;
         }
 
     }
